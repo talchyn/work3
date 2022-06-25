47385ae26a6e73de1b86640f272cfefb4988b5b7
talchyn <talchynerkinbekova@gmail.com>
2022-06-25 18:04:55 +0600

    Lesson 33

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,23 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         double[] arrey1 = {1.2, 2.2, -1.5, 1.7, -1.8, -2.1, 1.2, 2.2, 1.5, -1.7, 1.8, 2.1, -3.2, 3.3, 4.2};
-        int index = 0;
         double sum = 0.0;
+        double amount = 0.0;
         boolean on = false;
         for (double summedNumbers : arrey1) {
             if (summedNumbers < 0) {
                 on = true;
-            } else if (on == true) {
+            }
+            else if (on == true && summedNumbers > 0) {
                 sum += summedNumbers;
+                amount++;
+
 
             }
         }
-        System.out.println(sum);
+
+        System.out.println(sum / amount);
+
 
 
 
